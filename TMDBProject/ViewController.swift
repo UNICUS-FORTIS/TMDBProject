971534a1f6c25feda7e755567222f77ebb91395c
Louie Shin <unicus.fortis@gmail.com>
2023-08-12 16:56:52 +0900

    Initial Commit

---
+++ TMDBProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBProject
+//
+//  Created by LOUIE MAC on 2023/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
